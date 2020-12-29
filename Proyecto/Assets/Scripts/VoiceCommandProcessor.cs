c14690e48e6c0476d44724c2aea24b779053a129
LuisCrisantos <54858471+LuisCrisantos@users.noreply.github.com>
2020-12-28 20:01:56 -0800

    Commit Proyecto

---
+++ Proyecto/Assets/Scripts/VoiceCommandProcessor.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace IBM.Watsson.Examples{
+
+    public class VoiceCommandProcessor : MonoBehaviour
+    {
+        static protected VoiceCommandProcessor s_VoiceInstance;
+        static public VoiceCommandProcessor Instance {get{return s_VoiceInstance;}}
+        public delegate void OnVoiceCommand(string action);
+        public OnVoiceCommand onVoiceCommand;
+        public List<string> actions;
+        public List<string> specialActions;
+        public GameObject persona;
+        public List <GameObject> spawnPrefabs;
+        
+        void Awake()
+        {
+            s_VoiceInstance = this;
+        }
+
+        public void Create(string transcript)
+        {
+            string [] words = transcript.Split(' ');
+            foreach(var word in words)
+            {
+                if(actions.Contains(word.ToLower()))
+                {
+                    if(onVoiceCommand != null)
+                    {
+                        onVoiceCommand.Invoke(word.ToLower());
+                    }
+                    return;
+                }
+            }
+            foreach(var word in words)
+            {
+                if(specialActions.Contains(word.ToLower()))
+                {
+                    if(word == "invocar")
+                    {
+                        SpawnObject(words);
+                    }
+                }
+                break;
+            }                 
+        }
+
+        void SpawnObject(string[] words)
+        {
+            foreach(var word in words)
+            {
+                foreach(var prefab in spawnPrefabs)
+                {
+                    if(prefab.name == word.ToLower())
+                    {
+                        if(prefab.name == "sacerdotisa" || prefab.name == "ermitaño")
+                        {
+                            persona = Instantiate(prefab, new Vector3(0f,0.3f,-0.3f), Quaternion.identity);
+                        }
+                        else
+                        {
+                            persona = Instantiate(prefab, new Vector3(0f,0f,0f), Quaternion.identity);
+                        }                       
+                    }
+                    //break;
+                }
+                Destroy(persona,3);
+            }
+        }
+    }
+}
